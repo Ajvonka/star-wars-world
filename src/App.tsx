1e64ddfe393034c28b0e5b9c1a741da3554e77db
Ivona Gasparova <ivonga95@gmail.com>
2022-10-12 20:00:41 +0200

    Fetching and displaying data in list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+type Result = {
+  count: number;
+  next: string;
+  previous: string;
+  results: People[];
+};
+
+type People = {
+  name: string;
+  height: string;
+  mass: string;
+  gender: string;
+  url: string;
+};
+
+enum Url {
+  page = "https://swapi.py4e.com/api/people/",
+}
+
+const App = () => {
+  const [data, setData] = useState<Result>();
+
+  const getFetchedData = () => {
+    axios
+      .get(Url.page)
+      .then((res) => {
+        const data = res.data;
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getFetchedData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data?.results.map((result) => (
+          <li key={result.name}>
+            <img
+              src={`https://starwars-visualguide.com/assets/img/characters/${result.url
+                .replace(Url.page, "")
+                .replace("/", "")}.jpg`}
+            ></img>
+            {result.name} {result.height} {result.mass} {result.gender}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
